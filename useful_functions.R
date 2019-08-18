905787734adf95fee573bc8e3d5c510f4a275a4d
Wesley Oliveira <54210426+wessoliveira@users.noreply.github.com>
2019-08-18 14:22:56 -0300

    Create useful_functions.R

---
+++ useful_functions.R
@@ -0,0 +1,32 @@
+#
+memory_summary = function(object_names = NULL, sorting = "increasing"){
+  
+  objs = object_names
+  
+  if(is.null(object_names)) objs = as.character(ls(envir = .GlobalEnv))
+  
+  memory = lapply(
+    objs, function(x) eval(parse(text=paste0("object.size(", x, ")")))
+  )
+  
+  memory_bytes = as.numeric(memory)
+  
+  ord = 1:length(memory_bytes)
+  
+  if(sorting %in% c("decreasing", "increasing")) ord = order(
+    memory_bytes, 
+    decreasing = ifelse(sorting == "decreasing", TRUE, FALSE))
+  
+  memory_MB = lapply(
+    memory, function(x) format(x, standard = "SI", units = "MB"))
+  
+  types = sapply(
+    objs, function(x) eval(parse(text=paste0("class(", x, ")"))))
+  
+  result = data.frame(
+    object = objs, 
+    type = as.character(types), 
+    memory = as.character(memory_MB))
+  return(result[ord,])
+}
+#
